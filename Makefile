0fd7409564f18c240369a75b8c2e050473b4c3fb
Hemaditya Malla <hemadity@lanzarote.md.cwi.nl>
2019-12-03 13:38:24 +0100

    added a makefile and code runs

---
+++ Makefile
@@ -0,0 +1,27 @@
+FC := gfortran #This is the variable for the fortran compiler to be used
+FFLAGS := -o #These are the flags that we use while compilation
+RM := rm -f
+OBJ := sod_shock.o
+PROG := sod_shock
+OUTDIR := fortran_op
+
+.PHONY: all clean run
+
+
+all: $(PROG)
+
+clean: 
+	$(RM) $(PROG) *.o $(OUTDIR)/*
+
+run:
+	./$(PROG) || { echo "$(PROG) failed"; exit 1; };
+
+%.o: %.f90
+	$(FC) -c -o $@ $^
+
+$(PROG): $(OBJ) $(OUTDIR)
+	$(FC) $(FFLAGS) $@ $<
+
+
+$(OUTDIR):
+	mkdir -p $@
--- sod_shock.f90
+++ sod_shock.f90
@@ -5,7 +5,8 @@ program sodShock
         double precision, parameter :: tubeHalfLength = 1.0, Y = 1.4
         double precision, parameter :: Tfinal = 100.0  !This needs to be made clear
         double precision :: ul, ur, pl, pr, rhol, rhor, El, Er
-        double precision, allocatable, dimension(:) :: u, rho, E, p, x
+        double precision, allocatable, dimension(:) :: u, rho, E, p, x, unew, rhonew, Enew, pnew
+        double precision, allocatable, dimension(:) :: mflux, vflux, eflux
         integer :: i, N, iter, nGhost, ncv
         double precision :: xl, xr, dx, dt, t
 
@@ -18,12 +19,12 @@ program sodShock
         xr = tubeHalfLength
 
         N = int((xr - xl)/dx)
-        print *, N
         nGhost = 2
         ncv = N + nGhost
         !Initializing the arrays and setting initial conditions
 
-        allocate(x(N), u(ncv), rho(ncv),E(ncv), p(ncv))
+        allocate(x(N), u(ncv), rho(ncv),E(ncv), p(ncv), unew(ncv), rhonew(ncv),Enew(ncv), pnew(ncv))
+        allocate(mflux(N),vflux(N),eflux(N))
 
 
         !Initial conditions
@@ -40,6 +41,11 @@ program sodShock
         rho = 0.0
         E = 0.0
         p = 0.0
+        unew = 0.0
+        rhonew = 0.0
+        Enew = 0.0
+        pnew = 0.0
+
         do i=1,N
                 x(i) = xl + (dx + (i-1)*dx)
         end do
@@ -56,21 +62,72 @@ program sodShock
                         E(i+1) = Er
                 end if
         end do
-        !Take care of ghost cells
+        !Take care of ghost cells- we take that the tube is infinite- so we must make sure that the shock/expansion does not get too
+        !close to the bondaries
+        u(1) = ul
+        u(ncv) = ur
+        unew(1) = ul
+        unew(ncv) = ur
+
+        rho(1) = rhol
+        rho(ncv) = rhor
+        rhonew(1) = rhol
+        rhonew(ncv) = rhor
+
+        p(1) = pl
+        p(ncv) = pr
+        pnew(1) = pl
+        pnew(ncv) = pr
 
 
-        !Testing for initial conditions
+        E(1) = El
+        E(ncv) = Er
+        Enew(1) = El
+        Enew(ncv) = Er
         !Space derivative discretization
+        !mflux = (rho(2:ncv)*u(2:ncv) - rho(1:N+1)*u(1:N+1))/dx
+        !vflux = ((rho(2:ncv)*u(2:ncv)**2 + p(2:ncv)) - (rho(1:ncv-1)*u(1:ncv-1)**2 + p(1:ncv-1)))/dx
+        !eflux = ((E(2:ncv)*u(2:ncv) - p(2:ncv)*u(2:ncv)) - (E(1:ncv-1)*u(1:ncv-1) - p(1:ncv-1)*u(1:ncv-1)))/dx
 
-        !Time integration
+
+        !Time loop
+        t = 0.0
+        iter = 1
+        print *,'Starting time integration..'
+        do while (t < Tfinal)
+                
+                !Space derivative discretization
+                mflux = (rho(2:ncv)*u(2:ncv) - rho(1:N+1)*u(1:N+1))/dx
+                vflux = ((rho(2:ncv)*u(2:ncv)**2 + p(2:ncv)) - (rho(1:ncv-1)*u(1:ncv-1)**2 + p(1:ncv-1)))/dx
+                eflux = ((E(2:ncv)*u(2:ncv) - p(2:ncv)*u(2:ncv)) - (E(1:ncv-1)*u(1:ncv-1) - p(1:ncv-1)*u(1:ncv-1)))/dx
 
 
-        !Data writing
+                rhonew(2:ncv-1) = rho(2:ncv-1) + dt*(mflux)
+                unew(2:ncv-1) = (mflux + dt*vflux)/rhonew(2:ncv-1) !What if we divide by zero here?
+                Enew(2:ncv-1) = E(2:ncv-1) + dt*(eflux)
+                pnew(2:ncv-1) = (Y-1)*(Enew(2:ncv-1) + 0.5*rhonew(2:ncv-1)*unew(2:ncv-1)**2)
+
+                !Updating variables
+                rho = rhonew
+                u = unew
+                E = Enew
+                p = pnew
+                
+                
+                !Writing data
+                call writeData(iter, x, u(2:ncv-1), p(2:ncv-1), rho(2:ncv-1), N)
+                iter = iter+1
+                t = t + dt
+
+        end do
+        print *,'Finished!'
+
 
 
         !De-allocation of variable arrays
         deallocate(x)
-        deallocate(u, p, E, rho)
+        deallocate(u, p, E, rho, unew, pnew, Enew, rhonew)
+        deallocate(mflux, vflux, eflux)
 contains
 
         subroutine writeData(timeStep, x, u, p, rho, N)
@@ -80,7 +137,6 @@ contains
                 integer :: i
                 character(21) :: filename
                 write(filename, '(a,i0.6,a)') 'fortran_op/', timeStep, '.txt'
-                print *, 'testStuff'
                 open(1, file=filename, status='new')
                 write(1, *) 'x ', 'velocity ', 'rho ', 'pressure '
                 do i=1,N
