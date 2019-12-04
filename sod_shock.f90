6f3fe2d353e37df16568716eda2966ce6ae9779a
Hemaditya Malla <hemadity@lanzarote.md.cwi.nl>
2019-12-04 14:49:13 +0100

    Fully working code with Lax-Friedrichs solver and added more functions

--- sod_shock.f90
+++ sod_shock.f90
@@ -2,8 +2,8 @@ program sodShock
 
         implicit none
         !Parameters and variables
-        double precision, parameter :: tubeHalfLength = 10.0, Y = 1.4
-        double precision, parameter :: Tfinal = 0.01  !This needs to be made clear
+        double precision, parameter :: tubeHalfLength = 5.0, Y = 1.4
+        double precision, parameter :: Tfinal = 7e-3  !This needs to be made clear
         double precision :: ul, ur, pl, pr, rhol, rhor, El, Er
         double precision, allocatable, dimension(:) :: u, rho, E, p, x, unew, rhonew, Enew, pnew
         double precision, allocatable, dimension(:) :: mflux, vflux, eflux
@@ -11,9 +11,9 @@ program sodShock
         double precision :: xl, xr, dx, dt, t
 
 
-        dx = 0.4 !This needs to be made clear
+        dx = 0.025 !This needs to be made clear
 
-        dt = 4.276e-5 !This needs to be made clear
+        dt = (dx*0.1)/375.0 !This setting is based on the sound speed and a courant number of 0.2
 
         xl = -1.0*tubeHalfLength
         xr = tubeHalfLength
@@ -34,9 +34,9 @@ program sodShock
         rhor = 0.125
         pl = 1e5
         pr = 1e4
-        El = pl/(Y-1) + 0.5*rhol*ul**2
+        El = pl/(rhol*(Y-1)) + 0.5*rhol*ul**2 
 
-        Er = pr/(Y-1) + 0.5*rhor*ur**2
+        Er = pr/(rhor*(Y-1)) + 0.5*rhor*ur**2
         u = 0.0
         rho = 0.0
         E = 0.0
@@ -84,11 +84,6 @@ program sodShock
         E(ncv) = Er
         Enew(1) = El
         Enew(ncv) = Er
-        !Space derivative discretization
-        !mflux = (rho(2:ncv)*u(2:ncv) - rho(1:N+1)*u(1:N+1))/dx
-        !vflux = ((rho(2:ncv)*u(2:ncv)**2 + p(2:ncv)) - (rho(1:ncv-1)*u(1:ncv-1)**2 + p(1:ncv-1)))/dx
-        !eflux = ((E(2:ncv)*u(2:ncv) - p(2:ncv)*u(2:ncv)) - (E(1:ncv-1)*u(1:ncv-1) - p(1:ncv-1)*u(1:ncv-1)))/dx
-
 
         !Time loop
         t = 0.0
@@ -96,16 +91,12 @@ program sodShock
         print *,'Starting time integration..'
         do while (t < Tfinal)
                 
-                !Space derivative discretization
-                mflux = (rho(2:ncv)*u(2:ncv) - rho(1:N+1)*u(1:N+1))/dx
-                vflux = ((rho(2:ncv)*u(2:ncv)**2 + p(2:ncv)) - (rho(1:ncv-1)*u(1:ncv-1)**2 + p(1:ncv-1)))/dx
-                eflux = ((E(2:ncv)*u(2:ncv) + p(2:ncv)*u(2:ncv)) - (E(1:ncv-1)*u(1:ncv-1) + p(1:ncv-1)*u(1:ncv-1)))/dx
-
-
-                rhonew(2:ncv-1) = rho(2:ncv-1) - dt*(mflux)
-                unew(2:ncv-1) = (rho(2:ncv-1)*u(2:ncv-1) - dt*vflux)/rhonew(2:ncv-1) !What if we divide by zero here?
-                Enew(2:ncv-1) = E(2:ncv-1) - dt*(eflux)
-                pnew(2:ncv-1) = (Y-1)*(Enew(2:ncv-1) - 0.5*rhonew(2:ncv-1)*unew(2:ncv-1)**2)
+                call computeFluxes(dx, rho, u, p, E, mflux, vflux, eflux, N, nGhost)
+                
+                rhonew(2:ncv-1) = 0.5*(rho(3:ncv) + rho(1:N)) - 0.5*dt*(mflux)
+                unew(2:ncv-1) = (0.5*(rho(3:ncv)*u(3:ncv) + rho(1:N)*u(1:N)) - 0.5*dt*vflux)/rhonew(2:ncv-1) !What if we divide by zero here?
+                Enew(2:ncv-1) = 0.5*(E(1:ncv) + E(1:N)) - 0.5*dt*(eflux)
+                pnew(2:ncv-1) = computePressure(Enew(2:ncv-1), rhonew(2:ncv-1), unew(2:ncv-1), Y, size(unew(2:ncv-1)))!rhonew(2:ncv-1)*(Y-1)*(Enew(2:ncv-1) - 0.5*rhonew(2:ncv-1)*unew(2:ncv-1)**2)
 
                 !Updating variables
                 rho = rhonew
@@ -117,8 +108,8 @@ program sodShock
                 if (maxval(abs(rho)) .ge. 10) stop 'Solution diverging!'
                 
                 !Writing data
-                if (mod(iter,1) .le. 1e-15) then
-                        call writeData(iter, x, u(2:ncv-1), p(2:ncv-1), rho(2:ncv-1), N)
+                if (mod(iter,20) .le. 1e-15) then
+                        call writeData(iter, x, u(2:ncv-1), p(2:ncv-1), rho(2:ncv-1), E(2:ncv-1), N)
                 end if
                 iter = iter+1
                 t = t + dt
@@ -130,23 +121,60 @@ program sodShock
 
 
         !De-allocation of variable arrays
-        deallocate(x)
-        deallocate(u, p, E, rho, unew, pnew, Enew, rhonew)
-        deallocate(mflux, vflux, eflux)
+        !deallocate(x)
+        !deallocate(u, p, E, rho, unew, pnew, Enew, rhonew)
+        !deallocate(mflux, vflux, eflux)
 contains
 
-        subroutine writeData(timeStep, x, u, p, rho, N)
+        subroutine writeData(timeStep, x, u, p, rho, E, N)
                 implicit none
                 integer, intent(in) :: timeStep, N
-                double precision, dimension(N), intent(in) :: x, u, p, rho
+                double precision, dimension(N), intent(in) :: x, u, p, rho, E
                 integer :: i
                 character(21) :: filename
                 write(filename, '(a,i0.6,a)') 'fortran_op/', timeStep, '.txt'
                 open(1, file=filename, status='new')
-                write(1, *) 'x ', 'v ', 'rho ', 'p '
+                write(1, *) 'x ', 'v ', 'rho ', 'p ', 'E '
                 do i=1,N
-                        write(1, *) x(i), u(i), rho(i), p(i)
+                        write(1, *) x(i), u(i), rho(i), p(i), E(i)
                 end do
                 close(1)
         end subroutine writeData
+
+        subroutine computeFluxes(dx, rho, u, p, E, mflux, vflux, eflux, N, nghost)
+                implicit none
+                integer, intent(in) :: N, nghost
+                double precision, dimension(N+nghost), intent(in) :: u, p, E, rho
+                double precision, intent(in) :: dx
+                double precision, dimension(N), intent(out) :: mflux, vflux, eflux
+                integer :: ncv
+                ncv = N + nghost
+                mflux = (rho(3:ncv)*u(3:ncv) - rho(1:N)*u(1:N))/dx
+                vflux = ((rho(3:ncv)*u(3:ncv)**2 + p(3:ncv)) - (rho(1:N)*u(1:N)**2 + p(1:N)))/dx
+                eflux = ((E(3:ncv)*u(3:ncv) + p(3:ncv)*u(3:ncv)) - (E(1:N)*u(1:N) + p(1:N)*u(1:N)))/dx
+
+
+        end subroutine computeFluxes
+
+        function computeEnergy(p, Y, rho, u, arrsize) !Works only for arrays!
+                implicit none
+                double precision, intent(in) :: Y
+                integer, intent(in) :: arrsize
+                double precision, intent(in), dimension(arrsize) :: p, rho, u
+                double precision, dimension(arrsize) :: computeEnergy
+
+                computeEnergy = p/(rho*(Y-1)) + 0.5*rho*u**2
+                
+        end function computeEnergy
+
+        function computePressure(e, rho, u, Y, arrsize)
+                implicit none
+                double precision, intent(in) :: Y
+                integer, intent(in) :: arrsize
+                double precision, intent(in), dimension(arrsize) :: e, rho, u
+                double precision, dimension(arrsize) :: computePressure
+
+                computePressure = rho*(Y - 1)*(e - 0.5*rho*u**2)
+        end function computePressure
+
 end program sodShock
